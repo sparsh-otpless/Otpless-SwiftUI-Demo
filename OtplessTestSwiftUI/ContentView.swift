821a12fbd9df8a2aee20ecd56b8dc4290fb092d5
Sparsh Chadha <sparsh.chadha@otpless.com>
2023-11-30 11:45:39 +0530

    comments removed

--- OtplessTestSwiftUI/ContentView.swift
+++ OtplessTestSwiftUI/ContentView.swift
@@ -47,10 +47,10 @@ struct ContentView: View {
         //                showLoginPage = false
         //            }
         //        }
-        .overlay(showOtplessLoginPage)
+        .overlay(showOtplessLoginPageView)
     }
     
-    var showOtplessLoginPage: some View {
+    var showOtplessLoginPageView: some View {
         Group {
             if showLoginPage {
                 OtplessVCWrapperSwiftUI() { response in
@@ -61,45 +61,3 @@ struct ContentView: View {
         }
     }
 }
-
-//import SwiftUI
-//import OtplessSDK
-//
-//struct ContentView: View {
-//    @State var otplessREsp: OtplessResponse? = nil
-//    @State var isPresentingOtplessView = false
-//
-//    var body: some View {
-//        NavigationView {
-//            VStack {
-//                Image(systemName: "globe")
-//                    .imageScale(.large)
-//                    .foregroundStyle(.tint)
-//
-//                // Use NavigationLink for "Hello, world!" text
-//
-//
-//                // Display response data
-//                if let responseData = otplessREsp?.responseData?["data"] as? [String: Any] {
-//                    // Extract information from the responseData dictionary
-//                    let email = responseData["email"] as? [String: Any]
-//                    let waName = responseData["waName"] as? String
-//                    let mobile = responseData["mobile"] as? [String: Any]
-//
-//                    // Format the information as a string
-//                    let formattedText = """
-//                        Email: \(email?["email"] as? String ?? "")
-//                        Name: \(email?["name"] as? String ?? "")
-//                        WA Name: \(waName ?? "")
-//                        Mobile: \(mobile?["name"] as? String ?? "") - \(mobile?["number"] as? String ?? "Unable to get number")
-//                        """
-//
-//                    Text(formattedText)
-//                        .font(.body)
-//                        .padding()
-//                }
-//            }
-//            .padding()
-//        }
-//    }
-//}
